013da7006b60af295ed3c1dbff3b5f7c7e7b21f0
Sinan Bayar <sinanbayar88@gmail.com>
2024-12-07 22:00:31 +0300

    Add initial layout for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-white text-3xl underline">Home</h1>
-      <h1>deneme</h1>
+    <div className="flex h-screen max-h-screen">
+      {/* //TODO: OTP Verification | PasskeyModal */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="patient"
+            width={1000}
+            height={1000}
+            className="mb-12 h-10 w-fit"
+          />
+          {/* //TODO: Patient Form */}
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items text-dark-600 xl:text-left">
+              © {new Date().getFullYear()} CarePulse
+            </p>
+            <Link href="/?admin_true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        alt="patient"
+        width={1000}
+        height={1000}
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
